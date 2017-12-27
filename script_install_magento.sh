f6739bd78999e9a9a8d4b7a3145dc9e36f7f6a49
Sirwit Yeesong <sirawit@nipa.cloud>
2017-12-27 16:41:37 +0700

    upload magento zip

---
+++ script_install_magento.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#install php
+apt-get install php7.0-common php7.0-gd php7.0-mcrypt php7.0-curl php7.0-intl \
+php7.0-xsl php7.0-mbstring php7.0-zip php7.0-iconv mysql-client php7.0-soap -y
+
+#config apache
+a2enmod rewrite
+sed -i 's/DocumentRoot \/var\/www/DocumentRoot \/var\/www\/magento\//g' /etc/apache2/sites-available/000-default.conf
+sed -i "166s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
+systemctl restart apache2.service
+
+#prepare install magento
+mkdir /var/www/magento
+mv /tmp/Magento-CE.tar.gz /var/www/magento/
+cd /var/www/magento/
+tar -xvzf Magento-CE.tar.gz
+chown www-data:www-data -R /var/www/magento/
+
+#gen DB password
+MATRIX="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+LENGTH="8"
+    while [ "${n:=1}" -le "$LENGTH" ]
+    do
+        DB_PASSWORD="$DB_PASSWORD${MATRIX:$(($RANDOM%${#MATRIX})):1}"
+        let n+=1
+    done
+
+#create database
+mysql -u root  << EOF
+CREATE DATABASE magento;
+CREATE USER 'magento' IDENTIFIED BY '$DB_PASSWORD' ;
+GRANT ALL PRIVILEGES ON magento.* TO 'magento';
+quit
+EOF
+
+#write file DB config
+echo 'Database Server Host : localhost ' >> ~/install_config
+echo 'Database Server Username : magento ' >> ~/install_config
+echo 'Database Server Password : '$DB_PASSWORD' ' >> ~/install_config
+echo 'Database Name : magento ' >> ~/install_config
+echo 'Table prefix : (none) ' >> ~/install_config
+
+#finish
+echo 'DONE!!!!!!!!!!! :)'
