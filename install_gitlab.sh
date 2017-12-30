316b047746eb25d1de27a83a6e22fe30c59c73d9
Sirwit Yeesong <sirawit@nipa.cloud>
2017-12-30 15:08:22 +0700

    add install gitlab script

---
+++ install_gitlab.sh
@@ -0,0 +1,15 @@
+#!/ bin/bash
+
+#prepare
+apt-get update
+apt-get install -y curl openssh-server ca-certificates
+
+#install gitlab CE
+curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+sudo EXTERNAL_URL="http://gitlab.test.com" apt-get install gitlab-ce
+
+#initial config
+gitlab-ctl reconfigure
+
+
+
